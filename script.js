3cb3c0db7dbae3dff880293804b7e1ba01475368
vasara1987 <you@example.com>
2021-12-02 14:41:30 +0100

    baigti failai

---
+++ script.js
@@ -0,0 +1,53 @@
+let caracters = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+
+let firstElement = caracters[0];
+console.log(firstElement);
+
+let lastElement = caracters[caracters.length - 1];
+console.log(lastElement);
+
+
+myNumber = prompt("Įveskite skaičių nuo 1 iki 6");
+alert(caracters[myNumber-1]);
+
+
+//Vietoj šito
+
+// if (myNumber == 1) {
+//     alert(caracters[0]);
+// }
+
+// if (myNumber == 2) {
+//     alert(caracters[1]);
+// }
+
+// if (myNumber == 3) {
+//     alert(caracters[2]);
+// }
+
+// if (myNumber == 4) {
+//     alert(caracters[3]);
+// }
+
+// if (myNumber == 5) {
+//     alert(caracters[4]);
+// }
+
+// if (myNumber == 6) {
+//     alert(caracters[5]);
+// }
+
+
+
+
+
+alert(caracters[myNumber-1]);
+
+
+
+
+// caracters = prompt("Įveskite skaičių nuo 1 iki 6");
+
+// for (let i = 0; i = 1; i++) {
+//          alert(caracters[0])
+    // }
